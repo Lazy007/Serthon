475f5ba86a347d35207f5533ea2988fd557b1130
Lazy007 <105008830+Lazy007@users.noreply.github.com>
2023-03-28 16:45:45 +0000

    Patch 1.0.2

--- main.py
+++ main.py
@@ -1,20 +1,32 @@
-import socket
-import json
-import requests
-
-config = open("config.json", "r")
+import socket, json, requests, os, sys
+args = sys.argv[1:]
+if not args:
+  filename = "config.json"
+elif args[0] in ["-f", "--filename"]:
+  if not args[1]:
+    print("USAGE: python3 main.py -f filename")
+  else:
+    filename = args[1]
+elif args[0] in ["--help", "-h"]:
+  print("USAGE: python3 main.py [COMMAND] ")
+  print("                       -h, --help       shows this.")
+  print("                       -f, --filename   use to set config file name. DEFAULT: config.json")
+  exit()
+else:
+  print("USAGE: python3 main.py -f 'filename'")
+if not os.path.isfile(filename):
+  print(f"[ERROR] NO CONFIG FILE FOUND WITH NAME {filename}. CREATE ONE AS 'config.json' OR TO USE DIFFERENT FILENAME RUN SERTHON AS 'python3 main.py -f filename' ")
+  exit()
+config = open("filename", "r")
 config = config.read()
 config = json.loads(config)
 if not "server" in config:
-  print("[ERROR] CONFIG DOES NOT CONTAINS COMPLETE SERVER INFO. PLEASE ADD SEVER IN CONFIG FILE")
+  print("[ERROR] CONFIG DOES NOT CONTAINS SERVER INFO. PLEASE ADD SEVER IN CONFIG FILE")
   exit()
 else:
   server = config['server']
-if not server['address']:
-  print("[ERROR] CONFIG DOES NOT CONTAINS COMPLETE SERVER INFO. PLEASE ADD SEVER IN CONFIG FILE")
-  exit()
-if not server['port']:
-  print("[ERROR] CONFIG DOES NOT CONTAINS COMPLETE SERVER INFO. PLEASE ADD SEVER IN CONFIG FILE")
+if not "address" in server or not "port" in server:
+  print("[ERROR] CONFIG DOES NOT CONTAINS ADDRESS OR PORT INFO. PLEASE ADD SEVER IN CONFIG FILE")
   exit()
 if server['address'] == "public":
   try:
@@ -38,6 +50,9 @@ except TypeError:
   print("[ERROR] INVALID PORT OR IP. PLEASE CHANGE CONIFG")
   exit()
 except OSError:
+  if server['address'] == "local":
+    print("[ERROR] INVALID PORT OR IP. PLEASE CHANGE CONIFG")
+    exit()
   hostname = socket.gethostname()
   ip_address = socket.gethostbyname(hostname)
   HOST, PORT = ip_address, server['port']
@@ -47,12 +62,16 @@ except OSError:
   print("[ERROR] PORT OR IP ALREADY IN USE. PLEASE CHANGE CONFIG")
   exit()
 listen_socket.listen(1)
+
+
 def link(uri, label=None):
   if label is None:
     label = uri
   parameters = ''
   escape_mask = '\033]8;{};{}\033\\{}\033]8;;\033\\'
   return escape_mask.format(parameters, uri, label)
+
+
 linked = link(f"http://{ip_address}:{PORT}")
 print(f"Serving HTTP on \033[93m{linked}\033[00m\n")
 print("\033[96mLogs:\033[00m")
@@ -67,8 +86,8 @@ if "paths" in config:
 else:
   path_state = False
 while True:
-  client_connection, client_address = listen_socket.accept()
-  client_ip = client_address[0]
+  client_connection, client_ip = listen_socket.accept()
+  client_ip = client_ip[0]
   request_data = client_connection.recv(1024)
   split_data = request_data.decode().split()
   req_path = split_data[1:2][0]
